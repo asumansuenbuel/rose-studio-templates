465ef671996a1d880628f872c5d9fbd40f4656a4
Asuman <Suenbuel>
2019-01-13 20:43:38 +0000

    some initial changes

--- gazebo_fetch/scripts/start.sh
+++ gazebo_fetch/scripts/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+d=`dirname $0`
+ros_packages_dir="${d}/../ros_packages"
+export ROS_PACKAGE_PATH="${ROS_PACKAGE_PATH}:${ros_packages_dir}"
+
+echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH"
+
+
 clean_processes()
 {
     kill `ps aux|grep gazebo|grep fetch|awk '{print $2}'`
@@ -9,8 +16,16 @@ clean_processes
 
 sleep 2
 
-(sleep 10; xterm -e roslaunch my_fetch_gazebo_demo mydemo.launch) &
+(sleep 10; xterm -e ) &
 
-exec roslaunch my_fetch_gazebo playground.launch robot:=freight
+exec roslaunch my_fetch_gazebo playground.launch \
+//! switch(robot.shortName) {
+//! case 'FetchFreight':
+robot:=freight
+//!   break
+//! case 'Fetch':
+robot:=fetch
+//!   break
+//! }
 
 
---
+++ gazebo_fetch/scripts/start_demo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+
+echo "press enter when Gazebo UI is initialized..."
+
+read x
+
+roslaunch my_fetch_gazebo_demo mydemo.launch

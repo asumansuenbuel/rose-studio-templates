d352241e9889dad1626b9a8f57eda1bd97cfbf8d
Asuman <Suenbuel>
2019-01-14 18:24:04 +0000

    changes setting of ROS_PACKAGE_PATH in startup scripts

--- gazebo_fetch/scripts/start.sh
+++ gazebo_fetch/scripts/start.sh
@@ -2,7 +2,7 @@
 
 d=`dirname $0`
 ros_packages_dir="${d}/../ros_packages"
-export ROS_PACKAGE_PATH="${ROS_PACKAGE_PATH}:${ros_packages_dir}"
+export ROS_PACKAGE_PATH="${ros_packages_dir}:${ROS_PACKAGE_PATH}"
 
 echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH"
 

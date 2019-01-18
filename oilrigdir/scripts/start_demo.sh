c3c78b87f6bd67c85dcb871c50856cd4d3adf036
Asuman Suenbuel <asuman.suenbuel@sap.com>
2019-01-18 11:51:47 -0800

    created oilrigrir and content

---
+++ oilrigdir/scripts/start_demo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+
+d=`dirname $0`
+ros_packages_dir="${d}/../ros_packages"
+export ROS_PACKAGE_PATH="${ROS_PACKAGE_PATH}:${ros_packages_dir}"
+
+echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH"
+
+
+echo "press enter when Gazebo UI is initialized..."
+
+read x
+
+roslaunch my_fetch_gazebo_demo mydemo.launch

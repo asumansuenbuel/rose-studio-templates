b5fe11819b6bbc080956056baa3e3085ff211f6c
Asuman Suenbuel <asuman.suenbuel@sap.com>
2019-01-18 12:21:07 -0800

    copying fetchdemo to Oilrig

---
+++ Oilrig/scripts/start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+d=`dirname $0`
+ros_packages_dir="${d}/../ros_packages"
+export ROS_PACKAGE_PATH="${ros_packages_dir}:${ROS_PACKAGE_PATH}"
+
+echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH"
+
+
+clean_processes()
+{
+    kill `ps aux|grep gazebo|grep fetch|awk '{print $2}'`
+}
+
+clean_processes
+
+sleep 2
+
+//! if (runDemo) {
+(sleep 10; xterm -e sh $d/start_demo.sh) &
+//! }
+
+exec roslaunch my_fetch_gazebo playground.launch \
+//! switch(robot.shortName) {
+//! case 'FetchFreight':
+   robot:=freight
+//!   break
+//! case 'Fetch':
+   robot:=fetch
+//!   break
+//! }
+
+

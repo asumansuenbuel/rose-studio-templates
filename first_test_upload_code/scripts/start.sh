cfd0747df34426acbb45566f15897b590f128c07
Asuman Suenbuel <asuman.suenbuel@sap.com>
2020-01-01 23:10:48 -0800

    test commit from uploaded content

---
+++ first_test_upload_code/scripts/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+d=`dirname $0`
+ros_packages_dir="${d}/../ros_packages"
+export ROS_PACKAGE_PATH="${ros_packages_dir}:${ROS_PACKAGE_PATH}"
+
+echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH"
+
+
+clean_processes()
+{
+    kill `ps aux|grep gazebo|grep fetch|awk '{print $2}'`
+}
+
+clean_processes
+
+sleep 2
+
+(sleep 10; xterm -e sh $d/start_demo.sh) &
+
+exec roslaunch my_fetch_gazebo playground.launch \
+   robot:=fetch
+
+
+
